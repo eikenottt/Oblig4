982caf6492646bae2d1ebda52cf13321eca4e22b
eikenottt <rune.eikem@gmail.com>
2017-05-30 20:22:05 +0200

    + bug fix

--- src/no/uib/info233/v2017/rei008_jsi014/oblig4/GUI/GUI.java
+++ src/no/uib/info233/v2017/rei008_jsi014/oblig4/GUI/GUI.java
@@ -712,7 +712,7 @@ public class GUI{
             if (gameMaster.hasMoved(gameMaster.getGameID())) {
                 gameButtonsMultiplayer.makeClickable();
                 ((Timer)e.getSource()).stop();
-                gameMaster.gameProsessor(player);
+                gameMaster.gameProcessor(player);
                 Debugger.print("Your turn");
             }
         });
