b9cf5bfb0061f69b5bfe73c3e843973b6fe0445d
eikenottt <rune.eikem@gmail.com>
2017-05-31 11:43:30 +0200

    + testMultiplayer ( med Backup)

--- src/no/uib/info233/v2017/rei008_jsi014/oblig4/GameMaster.java
+++ src/no/uib/info233/v2017/rei008_jsi014/oblig4/GameMaster.java
@@ -105,18 +105,20 @@ public class GameMaster {
     public void listenToPlayerMove(Player player, int energyUse) { //TODO Listen for player move every 2 seconds
 
         if (!gameOver) { // Game Not Over
-
+            String[] playerMoves = new String[2];
             if (player1.getPulse() && player2.getPulse()) { // Both players are human -> MultiplayerGame
-
                 Queries.updateMove(this, player);
+                playerMoves = Queries.getPlayerMove(gameID);
 
 
             }
             if (player.equals(player1)) {
                 this.p1_energyUse = energyUse;  //TODO viska ut for at singleplayer skal fungere(player 1 ikke skal bli trukket 2x energy)
+                this.p2_energyUse = Integer.valueOf(playerMoves[1]);
                 System.out.println(player.getName() + " Used " + p1_energyUse + " -----------########");
             } else {
                 this.p2_energyUse = energyUse;
+                this.p1_energyUse = Integer.valueOf(playerMoves[0]);
                 System.out.println(player.getName() + " Used " + p2_energyUse + " -----------########");
             }
 
